95e24749abac4840039bb8713d9b28328e755ad7
leahchoi <soora913@gmail.com>
2018-08-23 15:52:20 -0700

    Pulling data from server and displaying to user

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import App from './components/app';
+import reduxPromise from 'redux-promise';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, {}, applyMiddleware(reduxPromise));
 ReactDOM.render(
     <Provider store={store}>
         <Router>
