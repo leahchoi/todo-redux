887f4922ff1696c45a88d4f7e70eadb6ad99304b
leahchoi <soora913@gmail.com>
2018-08-23 12:58:12 -0700

    still in progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,3 @@
 import types from './types';
+
+const API_KEY = '?key=lfz_data_soora';
--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
-import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-
+import {Route} from 'react-router-dom';
+import List from './list';
 
 const App = () => (
     <div className= 'container'>
         <h1 className="center">Redux To Do</h1>
+
+        <Route exact path='/' component={List}/>
     </div>
 );
 
