bd7012b4dfa07f314249f286d8271ed6c753ed53
leahchoi <soora913@gmail.com>
2018-08-29 11:58:55 -0700

    add item functioning

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,11 @@ export function getAllListData(){
         payload: resp
     };
 }
+export function addItem(item) {
+    const response = axios.post(`${BASE_URL}/todos${API_KEY}`, item);
+
+    return {
+        type: types.ADD_ITEM,
+        payload: response
+    }
+}
